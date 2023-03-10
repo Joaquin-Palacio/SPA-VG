17642fa312f851c08b78d028fa6af27e6dd3ac60
Joaquin <joaquinpalacio2311@gmail.com>
2023-03-10 15:10:26 -0300

    add models default

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,10 +3,40 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    'videogame',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      platforms: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+        defaultValue: [],
+      },
+      released: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
