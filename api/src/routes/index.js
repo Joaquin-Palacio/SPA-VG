44d19a8012435cbc6751cf23afcff699a6845c31
Joaquin <joaquinpalacio2311@gmail.com>
2023-03-13 12:48:29 -0300

    reset de 0

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,14 +2,10 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const genresRoute = require('../controllers/genreController');
-const platformsRoute = require('../controllers/platformController');
-
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/genres', genresRoute);
-router.use('/platforms', platformsRoute);
+
 
 module.exports = router;
