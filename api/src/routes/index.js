75aa5ec69df15a916791480ac5b58461f15afc24
Joaquin <joaquinpalacio2311@gmail.com>
2023-03-13 17:37:08 -0300

    fix index routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,17 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+
+const genres = require('../controllers/genreController');
+const platforms = require('../controllers/platformController');
+
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+/* router.use('/videogames', videogames); */
 
+router.use('/genres', genres);
+router.use('/platforms', platforms);
 
 module.exports = router;
